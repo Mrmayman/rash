8de6b8595125c90ffd4d7e2491f357f651737095
mrmayman <navneetkrishna22@gmail.com>
2024-04-22 18:04:24 +0530

    add Debug impl for VM types and CompilerError, add more errors to CompilerError

--- rash_loader_sb3/src/compiler/error.rs
+++ rash_loader_sb3/src/compiler/error.rs
@@ -1,13 +1,17 @@
 use super::variable_manager::{RegisterId, VariableIdentifier};
 
+#[derive(Debug)]
 pub enum RegisterAction {
     Freeing,
     Getting,
 }
 
+#[derive(Debug)]
 pub enum CompilerError {
     RegisterNotFound(RegisterId, RegisterAction),
     VariableNotFoundWhenGetting(VariableIdentifier),
     FieldInvalid(String),
     UnsupportedBlock(String),
+    UnsupportedHatBlock(String),
+    ChildBlockDoesNotReturn,
 }
--- rash_vm/src/bytecode/instructions.rs
+++ rash_vm/src/bytecode/instructions.rs
@@ -3,12 +3,12 @@ use crate::data_types::ScratchObject;
 /// A struct representing a point in the code to jump or goto to.
 /// Think of it like C goto.
 /// Mostly used for the compiling stage, as it is slow to run.
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct JumpPoint(pub usize);
 
 /// A pointer to data.
 /// It's just an index to the array of VM memory.
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DataPointer(pub usize);
 
 /// The bytecode instructions.
@@ -19,7 +19,7 @@ pub struct DataPointer(pub usize);
 /// * Whereas with Places, you define a place and give it an id, and jump to a place with that id. Think of it like C goto.
 /// * So the VM will have to search where that place is defined.
 /// * Places are easier to compile but slow to run, so we convert them to raw locations at the last stage.
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum Instruction {
     MemSetToValue {
         ptr: DataPointer,
